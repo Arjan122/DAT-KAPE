1bfa00b8d5b846c1074c307e9ae18c2148560cb8
Arjan122 <tuazon.arjan@gmail.com>
2024-05-21 12:32:11 +0800

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,23 @@
+function about(){
+    window.location.href="about.html";
+}
+
+function home(){
+    window.location.href="index.html";
+}
+
+function productlisting(){
+    window.location.href="productlisting.html";
+}
+
+function featuredproducts(){
+    window.location.href="featuredproducts.html";
+}
+
+function gallery(){
+    window.location.href="gallery.html";
+}
+
+function contacts(){
+    window.location.href="contacts.html";
+}
